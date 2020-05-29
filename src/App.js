87b06f0e563406f9d2572a56992b0a3ebd7607db
Alfonsina <alfonsina.l@gmail.com>
2020-05-28 21:21:23 -0400

    Added new components to start managing state with redux and built some of them, also added some changes to the app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 import './App.css';
 
 // Importar las paginas
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 //Importar firebase
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-// import { render } from 'node-sass';
-
+import {setCurrentUser} from './redux/user/user.actions';
+import {SelectCurrentUser} from './redux/user/user.selectors';
 //actualmente se trabaja mas con functional components porque son mas limpios y tienen un mejor manejo del state
 
 //Switch se encarga de manejar las rutas para mostrar cada pagina y con router se carga cada pagina. Detecta cada configuración de ruta a medida que se vaya cargando. Con switch se va cargando cada ruta
@@ -28,21 +31,20 @@ class App extends React.Component {
 
   //metodo para la carga del componente la primera vez
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           });
-          console.log(this.state);
         });
       }
-      this.setState({currentUser:userAuth});
+      setCurrentUser(userAuth);
     });
   }
 
@@ -54,15 +56,34 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
-    </div>  
+      </div>  
     )
   }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  currentUser: SelectCurrentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  //BrowserRouter funciona para las versiones nuevas y antiguas de react, mientras que el que trae por defecto solo funciona para las mas nuevas
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    {/* //BrowserRouter funciona para las versiones nuevas y antiguas de react, mientras que el que trae por defecto solo funciona para las mas nuevas */}
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
